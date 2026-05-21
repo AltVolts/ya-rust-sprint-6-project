4876fcaaae0f1151ef4681efb2f2f5e767b678bc
AltVolts <Kecheudir@yandex.ru>
2026-05-21 22:12:31 +0300

    - improve main.rs: replace unwrap() with "?", remove clone(), add some   error handling - code refactoring

--- src/main.rs
+++ src/main.rs
@@ -48,20 +48,30 @@
 //  -- Error
 //   --- нет сети
 //   --- отказано в доступе
-fn main() {
+use analysis::parse;
+use std::{env, fs};
+
+
+fn main() -> Result<(), Box<dyn std::error::Error>>  {
     println!("Placeholder для экспериментов с cli");
 
     let parsing_demo = r#"[UserBackets{"user_id":"Bob","backets":[Backet{"asset_id":"milk","count":3,},],},]"#.to_string();
-    let announcements = analysis::parse::just_parse_anouncements(parsing_demo).unwrap();
+    let announcements = parse::just_parse_anouncements(parsing_demo)?;
     println!("demo-parsed: {:?}", announcements);
 
-    let args = std::env::args().collect::<Vec<_>>();
-    let filename = args[1].clone();
-    println!("Trying opening file '{}' from directory '{}'", filename, std::env::current_dir().unwrap().to_string_lossy());
-    let file: std::rc::Rc<std::cell::RefCell<Box<dyn analysis::MyReader>>> = std::rc::Rc::new(std::cell::RefCell::new(Box::new(std::fs::File::open(filename).unwrap())));
+    let args:Vec<String> = env::args().collect();
+    let filename = args.get(1).ok_or("Usage: program <filename>")?;
 
-    let logs = analysis::read_log(file.clone(), analysis::READ_MODE_ALL, vec![]);
+    println!(
+        "Trying opening file '{}' from directory '{}'",
+        filename,
+        std::env::current_dir()?.display()
+    );
+    let file = fs::File::open(filename)?;
+
+    let logs = analysis::read_log(file, analysis::READ_MODE_ALL, vec![])?;
     println!("got logs:");
     logs.iter().for_each(|parsed| println!("  {:?}", parsed));
+    Ok(())
 }
 
